9673d6740c07b0ec9fa98c5f63b1b6aff2d80f19
amannot4u@gmail.com <amannot4u@gmail.com>
2015-10-13 19:16:52 +0530

    button events

--- my-scripts/content-frame.js
+++ my-scripts/content-frame.js
@@ -1,23 +1,53 @@
 
+function simulateClick(el) {
+    var evt;
+    if (document.createEvent) {
+        evt = document.createEvent("MouseEvents");
+        evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    }
+    (evt) ? el.dispatchEvent(evt) : (el.click && el.click());
+}
+
+// var foo = document.getElementById("hey");
+
+// foo.onclick = function () {alert("bar");}
+
+// simulateClick("hey");
+
 
 var onMessageListener = function(message, sender, sendResponse) {
 
-	console.log(message);
+	console.log(message, "atleast show thiss");
 	
+    // return;
+
     switch(message) {
         case "yt-next":
-        	console.log("next");
-            $(".ytp-next-button").trigger("click");
+            var el = document.getElementsByClassName("ytp-next-button")[0];
             break;
         case "yt-prev":
-            $(".ytp-prev-button").trigger("click");
+            var el = document.getElementsByClassName("ytp-prev-button")[0];
+
+            if(el.style.display == "none"){
+
+                var prev = history.state["spf-referer"];
+
+                if(prev.indexOf("youtube.com") > 0)
+                    window.history.back();
+                
+            }
+
             break;
         case "yt-pause-play":
-            $(".ytp-play-button").trigger("click");
+            var el = document.getElementsByClassName("ytp-play-button")[0];
             break;
     }
+
+    simulateClick(el);
+
     return true;
 }
 
 
 chrome.runtime.onMessage.addListener(onMessageListener);
+
