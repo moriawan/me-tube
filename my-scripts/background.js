03c46d4a1dd48bcebd0adbf0982da18be9b5438a
dango <amannot4u@gmail.com>
2016-08-21 16:45:10 +0530

    propagate state

--- my-scripts/background.js
+++ my-scripts/background.js
@@ -4,22 +4,32 @@
 
 //ytp-next-button ytp-button
 
-var dispatchCommand = function(command){
+var dispatchCommand = function(commandObj){
 
+    console.log(commandObj, chrome, chrome.runtime)
     if(chrome && chrome.runtime) {
-
         chrome.tabs.query({url: "*://www.youtube.com/*"}, function(tabs){
             tabs.forEach(function(tab, index){
-                console.log(tab, tab.id);
-
-                chrome.tabs.sendMessage(tab.id, command, function(){
-                    console.log("dispatched ",{"origin":"youtube+", "command":command}, "to", tab.title)
+                chrome.tabs.sendMessage(tab.id, commandObj, function(){
+                    console.log("dispatched ",{"ori gin":"youtube+", "command":commandObj}, "to", tab.title)
                 })
             })
         })
     }
 }
 
+var propogateState = function(stateObj){
+    
+    chrome.tabs.query({url: "*://*/*"}, function(tabs){
+        tabs.forEach(function(tab, index){
+            // console.log(tab, tab.id);
+
+            chrome.tabs.sendMessage(tab.id, stateObj, function(){
+                // console.log("dispatched ",{"origin":"youtube+", "command":command}, "to", tab.title)
+            })
+        })
+    })
+}
 
 chrome.topSites.get(function(urlList){
     console.log(urlList);
@@ -28,8 +38,7 @@ chrome.topSites.get(function(urlList){
 
 chrome.commands.onCommand.addListener(function(command) {
 
-    console.log("command", command);
-    dispatchCommand(command)
+    dispatchCommand({'type':'playerButton', 'command':command})
 
 });
 
@@ -39,10 +48,20 @@ chrome.commands.onCommand.addListener(function(command) {
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
 
-    console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+    // console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension", request);
+    
+    if(!(chrome && chrome.runtime)) {
+        console.log("chrome runtime error")
+        return;
+    }
 
     if (request.type == "playerButton"){
         dispatchCommand(request.command);
     }
+    else if(request.type == "propagate"){
+        propogateState(request)
+    }
 
-  });
+});
+
+// dispatchCommand({'type':'playerButton', 'command':'yt-next'})
---
+++ my-scripts/inject/command-reader.js
@@ -0,0 +1,68 @@
+// The ID of the extension we want to talk to.
+var metubeExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
+
+console.log("injected script", yt);
+
+onPlayerStateChange = function (state) {
+    sendMessageToMetube({type:"stateChange", message: {newState:state}});
+};
+
+
+sendMessageToMetube = function(messageObject){
+	var event = new CustomEvent('injectedEvent', { 'detail': messageObject });
+	document.dispatchEvent(event);
+}
+
+function dispatchCustomEvent(doc) {
+  
+}
+
+
+playerRef = yt.player.getPlayerByElement("player-api")
+
+playerRef.addEventListener("onStateChange", "onPlayerStateChange");
+
+var playerStatePropagate = setInterval(function() {
+	if (playerRef.isReady()) {
+
+		data = {
+			type:"propagate", 
+			message: {
+				duration: playerRef.getDuration(),
+				current: playerRef.getCurrentTime(),
+				title: playerRef.getCurrentVideoConfig().args.title,
+				thumbnailUrl: playerRef.getCurrentVideoConfig().args.thumbnail_url,
+				avgRating: playerRef.getCurrentVideoConfig().args.avg_rating,
+				buffered: playerRef.getProgressState().loaded,
+				state : playerRef.getPlayerState()
+			}
+		}
+
+		sendMessageToMetube(data)
+		// clearInterval(playerStatePropagate);
+	}
+}, 1000);
+
+
+// metubeEvent
+
+
+// document.addEventListener("metubeEvent", function(message) {
+//     console.log("message recieved from extension", message)
+
+//     switch(message.type){
+//         case "pause":
+//             break;
+//         case "play":
+//             break;
+//         case "seek":
+//             break;
+//         case "change":
+//             break;
+//         case "cue":
+//             break;
+
+//     }
+
+// });
+
